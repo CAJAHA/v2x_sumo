17e94f20a56e3893a5fabaf853232a1e8c4a6276
C <jiahouchu@gmail.com>
2023-07-10 05:24:56 +0800

    debug

--- generate-net.sh
+++ generate-net.sh
@@ -6,7 +6,7 @@ NET_FILE=${SAVE_DIR}/"net.xml"
 FLOW_FILE=${SAVE_DIR}/"flow.xml"
 ROUTE_FILE=${SAVE_DIR}/"route.xml"
 REROUTER_FILE=${SAVE_DIR}/"reroute.xml"
-netgenerate -g --grid.x-number 2 --grid.y-number 2 --grid.x-length 250 --grid.y-length 433 --default.speed 20.0 --o ${NET_FILE}
+netgenerate -g --grid.x-number 3 --grid.y-number 3 --grid.x-length 250 --grid.y-length 433 --default.speed 20.0 --o ${NET_FILE}
 python3 ${SUMO_HOME}tools/randomTrips.py -n ${NET_FILE} -o ${FLOW_FILE} --begin 0 --end 1 --flows 10 --jtrrouter --trip-attribute 'departPos="random" departSpeed="max"'
 jtrrouter --route-files=${FLOW_FILE} --net-file=${NET_FILE} --output-file=${ROUTE_FILE} --accept-all-destinations --allow-loops
 python3 ${SUMO_HOME}tools/generateContinuousRerouters.py -n ${NET_FILE} -o ${REROUTER_FILE}
